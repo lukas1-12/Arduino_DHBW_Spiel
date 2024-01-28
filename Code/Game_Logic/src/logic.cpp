451099229769891bf64f035d0fdfd69b176a2795
lukas1-12 <122440412+lukas1-12@users.noreply.github.com>
2024-01-28 17:02:07 +0000

    Updated the function to the new sketch of the track, track will be uploaded on OneDrive

--- Code/Game_Logic/src/logic.cpp
+++ Code/Game_Logic/src/logic.cpp
@@ -10,31 +10,31 @@ cla_session::cla_session(uint8_t _u8_player_quantity,
   u8_computer_quantity = _u8_computer_quantity;
   array_players[u8_player_quantity];
 
-    for (int i = 0; i < _u8_player_quantity; i++) {
-      array_players[i] = new cla_player(i, 5+i*10, this);
-    }
+  for (int i = 0; i < _u8_player_quantity; i++) {
+    array_players[i] = new cla_player(
+        i, 5 + i * 10, this); // ID = i, Startposition = 5 + i * 10
+  }
 };
 
-//Player(const std::string& name) : playerName(name), score(0) {}
-cla_player::cla_player(uint8_t _u8_player_id, uint8_t _u8_start_position, cla_session* _obj_my_session) {
+cla_player::cla_player(uint8_t _u8_player_id, uint8_t _u8_start_position,
+                       cla_session *_obj_my_session) {
   obj_my_session = _obj_my_session;
   u8_player_id = _u8_player_id;
   u8_start_position = _u8_start_position;
   for (int p = 0; p < 4; p++) {
-    u8_token_position[p] = p+1;
-  } 
+    u8_token_position[p] =
+        p + 1; // So each token has a unique position in the home field
+  }
 };
-
 
 std::array<uint8_t, 2>
 cla_session::Is_Occupied(uint8_t _u8_affected_track_position) {
   for (uint8_t i = 0; i < u8_player_quantity; i++) {
     for (uint8_t a = 0; a < 4; a++) {
-        if(array_players[i]->Get_Token_Position(a) == _u8_affected_track_position){
-
-        
-          return {i, a};
-        }
+      if (array_players[i]->Get_Token_Position(a) ==
+          _u8_affected_track_position) {
+        return {i, a}; // i stands for the player ID, a for the token number
+      }
     }
   }
   return {10, 0}; // 10 stands for no player at the given track position
@@ -44,7 +44,9 @@ bool cla_session::Return_Home(uint8_t _u8_affected_track_position) {
   if (Is_Occupied(_u8_affected_track_position).at(0) != 10) {
     array_players[Is_Occupied(_u8_affected_track_position).at(0)]
         ->Set_Token_Position(Is_Occupied(_u8_affected_track_position).at(1),
-                             Is_Occupied(_u8_affected_track_position).at(1));
+                             Is_Occupied(_u8_affected_track_position).at(1) +
+                                 1); // Set_Token_Position sets the token
+                                     // position to the home field
     return true;
   } else {
     return false;
@@ -53,19 +55,47 @@ bool cla_session::Return_Home(uint8_t _u8_affected_track_position) {
 
 uint8_t cla_player::Calculate_Possible_Position(uint8_t _u8_token_number,
                                                 uint8_t _u8_dice_value) {
-  return u8_token_position[_u8_token_number] + _u8_dice_value;
+  if (this->Get_Token_Progress(_u8_token_number) + _u8_dice_value >
+      39) { // token is on the track and would move into the finish
+    return u8_token_position[_u8_token_number] + _u8_dice_value +
+           u8_start_position + 5;
+  }
+  uint8_t u8_possible_position =
+      u8_token_position[_u8_token_number] + _u8_dice_value;
+  if (u8_possible_position >
+      44) { // token is on track and would move out of the track
+    u8_possible_position = u8_possible_position - 40;
+  }
+  return u8_possible_position;
 };
 
 uint8_t cla_player::Move_Token(uint8_t _u8_token_number,
                                uint8_t _u8_dice_value) {
-  uint8_t _u8_possible_position =
+  uint8_t u8_possible_position =
       Calculate_Possible_Position(_u8_token_number, _u8_dice_value);
-  if (obj_my_session->Is_Occupied(_u8_possible_position).at(0) == 10) {
-    u8_token_position[_u8_token_number] = _u8_possible_position;
-    return 0;
-  } else {
-
-    ;
+  if (obj_my_session->Is_Occupied(u8_possible_position).at(0) == 10) {
+    u8_token_position[_u8_token_number] = u8_possible_position;
+    return u8_possible_position;
+  } else if (obj_my_session->Is_Occupied(u8_possible_position).at(0) !=
+             u8_player_id) {
+    obj_my_session->Return_Home(u8_possible_position);
+    u8_token_position[_u8_token_number] = u8_possible_position;
+    return u8_possible_position;
+  } else if (obj_my_session->Is_Occupied(u8_possible_position).at(0) ==
+                 u8_player_id &&
+             u8_possible_position <
+                 45) { // token is on track and would not move into the finish
+    Move_Token(_u8_token_number, _u8_dice_value + 1);
+  } else if (obj_my_session->Is_Occupied(u8_possible_position).at(0) ==
+                 u8_player_id &&
+             u8_possible_position >
+                 44) { // token is on track and would move into the finish
+    return u8_token_position
+        [_u8_token_number]; // token stays on the same position because other
+                            // token of the same player is in the way
+  }
+  else {
+    return 0; // Error
   }
 };
 
@@ -80,7 +110,17 @@ uint8_t cla_player::Set_Token_Position(uint8_t _u8_token_number,
 };
 
 uint8_t cla_player::Get_Token_Progress(uint8_t _u8_token_number) {
-  return u8_token_position[_u8_token_number] - u8_start_position;
+  if (u8_token_position[_u8_token_number] >= 5) { 
+    int i_token_progress =
+        u8_token_position[_u8_token_number] - u8_start_position;
+    if (i_token_progress < 0) {
+      i_token_progress =
+          u8_token_position[_u8_token_number] + 40 - u8_start_position;
+    }
+    return i_token_progress;
+  } else {
+    return 0;
+  }
 };
 
 status cla_player::Get_Status(uint8_t _u8_token_number) {
@@ -133,22 +173,12 @@ uint8_t cla_player::Get_Player_Progress() {
 
 bool Is_Computer() { return false; };
 
-uint8_t cla_computer_player::Auto_Move(uint8_t _u8_token_number){
-    return 0;
-};
+uint8_t cla_computer_player::Auto_Move(uint8_t _u8_token_number) { return 0; };
 
-uint8_t cla_manual_player::Manual_Move(uint8_t _u8_token_number){
-    return 0;
-};
+uint8_t cla_manual_player::Manual_Move(uint8_t _u8_token_number) { return 0; };
 
-uint8_t cla_session::Get_Player_Quantity(){
-  return u8_player_quantity;
-};
+uint8_t cla_session::Get_Player_Quantity() { return u8_player_quantity; };
 
-uint8_t cla_player::Get_Start_Position(){
-  return u8_start_position;
-};
+uint8_t cla_player::Get_Start_Position() { return u8_start_position; };
 
-uint8_t cla_player::Get_Player_ID(){
-  return u8_player_id;
-};
+uint8_t cla_player::Get_Player_ID() { return u8_player_id; };
--- Code/Game_Logic/src/main.cpp
+++ Code/Game_Logic/src/main.cpp
@@ -5,15 +5,21 @@
 
 int main() {
   cla_session game(4, 0);
-  std::cout <<"Test"<< std::endl;
-  std::cout << (int)game.Get_Player_Quantity() << std::endl;
-  std::cout << (int)game.array_players[1]->Get_Token_Position(3) << std::endl;
-  std::cout << (int)game.array_players[1]->Get_Start_Position() << std::endl;
-  std::cout << (int)game.array_players[0]->Get_Player_ID() << std::endl;
-  std::cout << (int)game.array_players[2]->Get_Start_Position() << std::endl;
-  std::cout << "Hello World!" << std::endl;
+  std::cout << "###############################################" << std::endl;
   game.array_players[1]->Set_Token_Position(3, 10);
+  //std::cout << game.Return_Home(10) << std::endl;
+  game.array_players[1]->Move_Token(3, 5);
+  std::cout << (int)game.array_players[1]->Get_Token_Position(3) << std::endl;
+  game.array_players[0]->Set_Token_Position(0, 10);
   std::cout << (int)game.Is_Occupied(10).at(0) << std::endl;
   std::cout << (int)game.Is_Occupied(10).at(1) << std::endl;
+  std::cout << "###############################################" << std::endl;
+  game.array_players[3]->Set_Token_Position(1, 30);
+  std::cout << (int)game.array_players[3]->Get_Token_Position(1) << std::endl;
+  std::cout << (int)game.array_players[1]->Get_Token_Position(3) << std::endl;
+  std::cout << "###############################################" << std::endl;
+  std::cout << (int)game.array_players[3]->Get_Token_Progress(1) << std::endl;
+  std::cout << (int)game.array_players[3]->Calculate_Possible_Position(1, 5) << std::endl;
+
   return 0;
 }
