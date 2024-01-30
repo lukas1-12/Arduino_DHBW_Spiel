51daf0e3e2131795d5abe9a5611e452b079f46eb
Jonathan Schulz <jonathanschulz@MBPvonJonathan.fritz.box>
2024-01-30 23:16:42 +0100

    Added player quantity input logic and Dice roll.

--- Engineering/COD_ENG_OUT_arduino_code/src/main.cpp
+++ Engineering/COD_ENG_OUT_arduino_code/src/main.cpp
@@ -21,7 +21,7 @@
 #define WHITE_BRIGHT 0xffff
 
 volatile ASL::en_state en_current_state = ASL::setup_real_players;
-uint8_t u8_player_quantity = 0;
+uint8_t u8_player_quantity = 1;
 uint8_t u8_computer_quantity = 0;
 uint8_t u8_current_token_number = 0;
 ASL::cla_display obj_display(A, B, C, CLK, LAT, OE);
@@ -33,30 +33,41 @@ void setup() {
   obj_display.Set_Colors(3, GREEN_BRIGHT, GREEN_DARK);
   obj_display.Begin();
   ASL::Setup_Buttons();
+  ASL::Setup_Dice();
 }
 uint8_t u8_test_incrementer = 0;
 
 void loop() {
   switch (en_current_state) {
   case ASL::setup_real_players:
-
+    obj_display.Display_Players(u8_player_quantity);
     break;
   case ASL::modify_real_player_number:
-
+    if (u8_player_quantity < 4) {
+      u8_player_quantity++;
+    } else {
+      u8_player_quantity = 1;
+    }
     en_current_state = ASL::setup_real_players;
     break;
   case ASL::setup_computer_players:
-
+    obj_display.Display_Players(u8_player_quantity);
     break;
   case ASL::modify_computer_player_number:
-
+    if (u8_player_quantity < 4) {
+      u8_player_quantity++;
+      u8_computer_quantity++;
+    } else {
+      u8_player_quantity = u8_player_quantity - u8_computer_quantity;
+      u8_computer_quantity = 0;
+    }
     en_current_state = ASL::setup_computer_players;
     break;
   case ASL::wait_for_dice_roll:
-
+    // NOP
     break;
   case ASL::roll_the_dice:
-
+    obj_display.Display_Dice(ASL::Roll_Dice());
     en_current_state = ASL::display_token;
     break;
   case ASL::wait_for_player_input:
@@ -78,5 +89,4 @@ void loop() {
     en_current_state = ASL::setup_real_players;
     break;
   }
-  delay(1000);
 }
