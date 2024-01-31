80f246a5762797244d815969f24d65d1f891bb8d
lukas1-12 <122440412+lukas1-12@users.noreply.github.com>
2024-01-31 12:32:16 +0000

    Made further progress in Calculate Possible Position and Get Token Progress methods, it is now safer and more reliable but still need to be tested and improved

--- Code/Game_Logic/src/logic.cpp
+++ Code/Game_Logic/src/logic.cpp
@@ -46,7 +46,7 @@ bool cla_session::Return_Home(uint8_t _u8_affected_track_position) {
         ->Set_Token_Position(Is_Occupied(_u8_affected_track_position).at(1),
                              Is_Occupied(_u8_affected_track_position).at(1) +
                                  1); // Set_Token_Position sets the token
-                                     // position to the home field
+                                     // position to the home field (1,2,3,4)
     return true;
   } else {
     return false;
@@ -55,37 +55,67 @@ bool cla_session::Return_Home(uint8_t _u8_affected_track_position) {
 
 uint8_t cla_player::Calculate_Possible_Position(uint8_t _u8_token_number,
                                                 uint8_t _u8_dice_value) {
-  if (this->Get_Token_Progress(_u8_token_number) + _u8_dice_value >
-      39) { // token is on the track and would move into the finish
-    return u8_token_position[_u8_token_number] + _u8_dice_value +
-           u8_start_position + 5;
+  if (u8_token_position[_u8_token_number] <= 4 &&
+      _u8_dice_value ==
+          6) { // token is in the home field and would move onto the track
+    return u8_start_position;
+  } else if (u8_token_position[_u8_token_number] <= 4 &&
+             _u8_dice_value != 6) { // token is in the home field and would not
+                                    // move onto the track
+    return u8_token_position[_u8_token_number];
+  } else if (this->Get_Token_Progress(_u8_token_number) + _u8_dice_value > 39 &&
+             this->Get_Token_Progress(_u8_token_number) + _u8_dice_value <
+                 44) { // token is on the track and would move into the finish
+    return Get_Token_Progress(_u8_token_number) + _u8_dice_value + 5;
+  } else if (u8_token_position[_u8_token_number] > 44 &&
+             u8_token_position[_u8_token_number] + _u8_dice_value < 49) {
+    // token is in the finish and would not move out of the finish
+    return u8_token_position[_u8_token_number] + _u8_dice_value;
+  } else if (u8_token_position[_u8_token_number] > 44 &&
+             u8_token_position[_u8_token_number] + _u8_dice_value > 49) {
+    // token is in the finish and would move out of the finish
+    return u8_token_position[_u8_token_number];
+  } else {
+    uint8_t u8_possible_position =
+        u8_token_position[_u8_token_number] + _u8_dice_value;
+    if (u8_possible_position >
+        44) { // token is on track and would move out of the track
+      u8_possible_position = u8_possible_position - 40;
+    }
+    return u8_possible_position;
   }
-  uint8_t u8_possible_position =
-      u8_token_position[_u8_token_number] + _u8_dice_value;
-  if (u8_possible_position >
-      44) { // token is on track and would move out of the track
-    u8_possible_position = u8_possible_position - 40;
-  }
-  return u8_possible_position;
 };
 
 uint8_t cla_player::Move_Token(uint8_t _u8_token_number,
                                uint8_t _u8_dice_value) {
   uint8_t u8_possible_position =
       Calculate_Possible_Position(_u8_token_number, _u8_dice_value);
-  if (obj_my_session->Is_Occupied(u8_possible_position).at(0) == 10) {
+  if (u8_possible_position ==
+      Get_Token_Position(
+          _u8_token_number)) { // token stays on the same position (e.g would
+                               // move out of the finish)
+    return Get_Token_Position(_u8_token_number);
+  } else if (obj_my_session->Is_Occupied(u8_possible_position).at(0) ==
+             10) { // possible position is not occupied
     u8_token_position[_u8_token_number] = u8_possible_position;
     return u8_possible_position;
   } else if (obj_my_session->Is_Occupied(u8_possible_position).at(0) !=
-             u8_player_id) {
+                 u8_player_id &&
+             obj_my_session->Is_Occupied(u8_possible_position).at(0) != 10) {
+    // possible position is occupied by another player -> other player gets sent
+    // home
     obj_my_session->Return_Home(u8_possible_position);
     u8_token_position[_u8_token_number] = u8_possible_position;
     return u8_possible_position;
   } else if (obj_my_session->Is_Occupied(u8_possible_position).at(0) ==
                  u8_player_id &&
              u8_possible_position <
-                 45) { // token is on track and would not move into the finish
+                 45) { // token is on track, position is occupied by other token
+                       // of same player and would not move into the finish
+                       // -> other token of the same player gets moved one field
+                       // further
     Move_Token(_u8_token_number, _u8_dice_value + 1);
+    return 0; // error if Move_Token(.... _u8_dice_value+1) is not called
   } else if (obj_my_session->Is_Occupied(u8_possible_position).at(0) ==
                  u8_player_id &&
              u8_possible_position >
@@ -93,8 +123,7 @@ uint8_t cla_player::Move_Token(uint8_t _u8_token_number,
     return u8_token_position
         [_u8_token_number]; // token stays on the same position because other
                             // token of the same player is in the way
-  }
-  else {
+  } else {
     return 0; // Error
   }
 };
@@ -110,14 +139,19 @@ uint8_t cla_player::Set_Token_Position(uint8_t _u8_token_number,
 };
 
 uint8_t cla_player::Get_Token_Progress(uint8_t _u8_token_number) {
-  if (u8_token_position[_u8_token_number] >= 5) { 
-    int i_token_progress =
+  if (u8_token_position[_u8_token_number] >= 5) { // token is on the track
+    int8_t i8_token_progress =
         u8_token_position[_u8_token_number] - u8_start_position;
-    if (i_token_progress < 0) {
-      i_token_progress =
+    if (i8_token_progress < 0) { // to compensate the offset for each player
+      i8_token_progress =
           u8_token_position[_u8_token_number] + 40 - u8_start_position;
     }
-    return i_token_progress;
+    if (u8_token_position[_u8_token_number] >= 44) {
+      i8_token_progress =
+          u8_token_position[_u8_token_number] -
+          u8_start_position; // to compensate the offset on the field
+    }
+    return i8_token_progress;
   } else {
     return 0;
   }
@@ -163,12 +197,12 @@ status cla_player::Get_Status(uint8_t _u8_token_number) {
 };
 
 uint8_t cla_player::Get_Player_Progress() {
-  uint8_t _u8_overall_progress = 0;
+  uint8_t u8_overall_progress = 0;
   for (uint8_t u8_token_number = 0; u8_token_number < 4; u8_token_number++) {
-    _u8_overall_progress +=
+    u8_overall_progress +=
         this->cla_player::Get_Token_Progress(u8_token_number);
   }
-  return _u8_overall_progress;
+  return u8_overall_progress;
 };
 
 bool Is_Computer() { return false; };
--- Code/Game_Logic/src/main.cpp
+++ Code/Game_Logic/src/main.cpp
@@ -4,22 +4,56 @@
 
 
 int main() {
-  cla_session game(4, 0);
-  std::cout << "###############################################" << std::endl;
-  game.array_players[1]->Set_Token_Position(3, 10);
-  //std::cout << game.Return_Home(10) << std::endl;
-  game.array_players[1]->Move_Token(3, 5);
-  std::cout << (int)game.array_players[1]->Get_Token_Position(3) << std::endl;
-  game.array_players[0]->Set_Token_Position(0, 10);
-  std::cout << (int)game.Is_Occupied(10).at(0) << std::endl;
-  std::cout << (int)game.Is_Occupied(10).at(1) << std::endl;
-  std::cout << "###############################################" << std::endl;
-  game.array_players[3]->Set_Token_Position(1, 30);
-  std::cout << (int)game.array_players[3]->Get_Token_Position(1) << std::endl;
-  std::cout << (int)game.array_players[1]->Get_Token_Position(3) << std::endl;
-  std::cout << "###############################################" << std::endl;
-  std::cout << (int)game.array_players[3]->Get_Token_Progress(1) << std::endl;
-  std::cout << (int)game.array_players[3]->Calculate_Possible_Position(1, 5) << std::endl;
+  cla_session game(2, 0);
+  game.array_players[1]->Set_Token_Position(0, 44); 
+  game.array_players[1]->Set_Token_Position(1, 43);
+  //std::cout << "Start Position Player 1: " << (int)game.array_players[1]->Get_Start_Position() << std::endl;
+  std::cout << "Token 0 progress: " << (int)game.array_players[1]->Get_Token_Progress(0) << std::endl;
+  std::cout << "Token 0 could be at Position: " << (int)game.array_players[1]->Calculate_Possible_Position(0, 3) << std::endl;
+  game.array_players[1]->Move_Token(0,3);
+  std::cout << "Token 0 at Position: " << (int)game.array_players[1]->Get_Token_Position(0) << std::endl;
+  game.array_players[1]->Move_Token(1,5);
+  std::cout << "Token 1 at Position: " << (int)game.array_players[1]->Get_Token_Position(1) << std::endl;
 
-  return 0;
+
+
+  /*
+  std::cout << (int)game.array_players[0]->Get_Start_Position() << std::endl;
+  std::cout << (int)game.array_players[1]->Get_Start_Position() << std::endl;
+  std::cout << "###############################################" << std::endl;
+  std::cout << (int)game.Get_Player_Quantity() << " Players" << std::endl;
+  int8_t i8_game = 1;
+  int8_t i8_player = 0;
+  while(i8_game) {
+    std::cout << "Player " << (int)i8_player << " is on turn" << std::endl;
+    for(int w = 0; w < 4; w++) {
+      std::cout << "Token " << w << " is on position " << (int)game.array_players[i8_player]->Get_Token_Position(w) << std::endl;
+    }
+    int dice_value = 0;
+    std::cout << "Please roll the dice:" << std::endl;
+    std::cin >> dice_value;
+    if(dice_value > 6) {
+      i8_game = 0;
+      break;
+    }
+    for(int w = 0; w < 4; w++) {
+      std::cout << "Token " << w << " could be on position " << (int)game.array_players[i8_player]->Calculate_Possible_Position(w, dice_value) << std::endl;
+    }
+    int token_number = 0;
+    std::cin >> token_number;
+    game.array_players[i8_player]->Move_Token(token_number, dice_value);
+    std::cout << "Token " << (int)token_number << " is now on position " << (int)game.array_players[i8_player]->Get_Token_Position(token_number) << std::endl;
+    if(game.array_players[i8_player]->Get_Player_Progress() == 166){
+      i8_game = 0;
+    };
+    if(i8_player == 0){
+    i8_player = 1;
+    } else {
+      i8_player = 0;
+    }
+    std::cout << "-----------------------------------------------" << std::endl;
+  }
+    std::cout << "###############################################" << std::endl;
+    std::cout << "Game finished" << std::endl;
+  */
 }
