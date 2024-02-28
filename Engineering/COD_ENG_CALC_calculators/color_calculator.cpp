52f64cc5e8cf6b6fbcb94bd11c33d615eebbf790
Jonathan Schulz <jonathanschulz@MBPvonJonathan.fritz.box>
2024-02-28 19:12:52 +0100

    Added Doxygen to calculator

--- Engineering/COD_ENG_CALC_calculators/color_calculator.cpp
+++ Engineering/COD_ENG_CALC_calculators/color_calculator.cpp
@@ -1,6 +1,32 @@
+/**
+ * @file color_calculator.cpp
+ * @brief This file contains the implementation of the color calculator.
+ *
+ * The color calculator is used to calculate the 16 bit color value from RGB
+ * values. The 16 Bit intgeger can then be used for the Matrix library directly.
+ * This reduces the startuptime of the arduino compared to setting the RGB
+ * values in the Setup routine.
+ *
+ * The color calculator expects the Values in RGB order. Values can reach from 0
+ * to 255. Be careful as You will NOT be told when input is invalid.
+ *
+ * compile with: ```gcc color_calculator.cpp -o color_calculator```
+ *
+ * Run with: ```./color_calculator```
+ */
+
 #include <inttypes.h>
 #include <stdio.h>
 
+/**
+ * @brief This function calculates the 16 bit color value from the given RGB
+ * values.
+ *
+ * @param r The red value of the color.
+ * @param g The green value of the color.
+ * @param b The blue value of the color.
+ * @return The 16 bit color value to be used for the LED matrix.
+ */
 uint16_t getColor(uint8_t r, uint8_t g, uint8_t b) {
   return ((uint16_t)(r & 0xF8) << 8) | ((uint16_t)(g & 0xFC) << 3) | (b >> 3);
 }
